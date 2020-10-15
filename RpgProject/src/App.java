6a294ddd136a13fe30e49f1c40042d6d2fd83029
JohnLest <j_schwickerath@hotmail.com>
2020-10-15 15:09:28 +0200

    Changement d'archi : Maeven et le multi-tiers

--- RpgProject/src/App.java
+++
@@ -1,8 +0,0 @@
-import utils.ConfigFile;
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println(ConfigFile.config("name"));
-        System.out.println(ConfigFile.config("version"));
-    }
-}
