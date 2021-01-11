134e82c4ba96e5ff3b6535d82fb7f274c4490ab7
JohnLest <j_schwickerath@hotmail.com>
2021-01-11 16:32:42 +0100

    Fin de l'analyse

--- RpgProject/Rpg-App/src/main/java/masi/rpg/app/App.java
+++ RpgProject/Rpg-App/src/main/java/masi/rpg/app/App.java
@@ -5,7 +5,6 @@ import masi.rpg.database.*;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! Main");
         Connection connect = Connect.connect();
         new Controler(connect);
         Disconnect.disconnect(connect);
